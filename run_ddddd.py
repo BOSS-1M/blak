282710a59ee07b1c71944c2521d1fc69ec9c0d50
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-05-17 17:01:59 +0100

    Add files via upload

---
+++ run_ddddd.py
@@ -0,0 +1,9 @@
+
+# ملف تشغيل للوحدة المشفرة ddddd.cpython-312.so
+import ddddd
+
+# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
+if hasattr(ddddd, 'main'):
+    ddddd.main()
+else:
+    print("تم استيراد ddddd بنجاح، ولكن لا توجد دالة main للتشغيل.")
