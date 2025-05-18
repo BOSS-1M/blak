32ffcfb5d0d453ca9f79ebf1f095dd4428174540
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-05-18 09:19:45 +0100

    Delete run_ddddd.py

--- run_ddddd.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة ddddd.cpython-312.so
-import ddddd
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(ddddd, 'main'):
-    ddddd.main()
-else:
-    print("تم استيراد ddddd بنجاح، ولكن لا توجد دالة main للتشغيل.")
