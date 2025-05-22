f337fb4973bad876c2fb782f24b17fc98ff815c7
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-05-22 10:07:44 +0100

    Delete venom.py

--- venom.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة venom.cpython-312.so
-import venom
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(venom, 'main'):
-    venom.main()
-else:
-    print("تم استيراد venom بنجاح، ولكن لا توجد دالة main للتشغيل.")
