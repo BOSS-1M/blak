7c242b781d02726257b9a490ee0ec0ce461dbf82
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-06-22 18:19:16 +0100

    Delete red_fixed.py

--- red_fixed.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة red_fixed.cpython-312.so
-import red_fixed
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(red_fixed, 'main'):
-    red_fixed.main()
-else:
-    print("تم استيراد red_fixed بنجاح، ولكن لا توجد دالة main للتشغيل.")
