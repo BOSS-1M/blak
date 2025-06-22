1ad1db9b96b1c14967e456aa72a462baf3a48b35
👑 𝐌𝐑 𝐑𝐈𝐂𝐎 👑 <max2005alg@gmail.com>
2025-06-22 18:27:31 +0100

    Delete run_red_fixed_ready.py

--- run_red_fixed_ready.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة red_fixed_ready.cpython-312.so
-import red_fixed_ready
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(red_fixed_ready, 'main'):
-    red_fixed_ready.main()
-else:
-    print("تم استيراد red_fixed_ready بنجاح، ولكن لا توجد دالة main للتشغيل.")
